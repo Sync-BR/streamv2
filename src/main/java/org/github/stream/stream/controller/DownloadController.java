aadf752938eb2c919aa99f861e70eb0566d236f9
eduardo <eduardofreitasfr@gmail.com>
2026-05-21 17:08:38 -0300

    feat:correção

--- src/main/java/org/github/stream/stream/controller/DownloadController.java
+++ src/main/java/org/github/stream/stream/controller/DownloadController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.InputStream;
 import java.net.http.HttpResponse;
 
-@RestController
+@Controller
 public class DownloadController {
 
     private final RequestUtil util;
