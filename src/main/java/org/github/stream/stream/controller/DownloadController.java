217d8513d96341685df8534fb2dbf1969f4e57da
eduardo <eduardofreitasfr@gmail.com>
2026-05-21 18:18:39 -0300

    feat? update

--- src/main/java/org/github/stream/stream/controller/DownloadController.java
+++ src/main/java/org/github/stream/stream/controller/DownloadController.java
@@ -3,7 +3,6 @@ package org.github.stream.stream.controller;
 import org.github.stream.stream.util.RequestUtil;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.net.http.HttpResponse;
 
 @Controller
@@ -28,27 +26,15 @@ public class DownloadController {
     public ResponseEntity<InputStreamResource> downloadVideo(@PathVariable Long streamId) throws Exception {
 
         HttpResponse<InputStream> response = util.createResponse(streamId);
-        try{
 
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-        if (response.statusCode() != 200) {
-            return ResponseEntity.status(response.statusCode()).build();
-        }
+//        if (response.statusCode() != 200) {
+//            return ResponseEntity.status(response.statusCode()).build();
+//        }
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"filme-" + streamId + ".mp4\"")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(new InputStreamResource(response.body()));
-    }
-    @GetMapping("/download/{streamId}")
-    public ResponseEntity<Void> download(@PathVariable Long streamId) {
-        String urlFinal = "https://uexme.pics/movie/34236517/83156563/15996576.mp4";
-
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .location(URI.create(urlFinal))
-                .build();
     }
 
 //    @GetMapping("/download-video")
--- src/main/java/org/github/stream/stream/util/RequestUtil.java
+++ src/main/java/org/github/stream/stream/util/RequestUtil.java
@@ -40,10 +40,8 @@ public class RequestUtil {
 
         return HttpRequest.newBuilder()
                 .uri(URI.create(serverPanel+streamId+".mp4"))
-                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
-                .header("Accept-Language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7")
-                .header("Connection", "keep-alive")
+                .header("User-Agent", "Mozilla/5.0")
+                .header("Accept", "*/*")
                 .GET()
                 .build();
     }
